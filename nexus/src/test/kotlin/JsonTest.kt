e860dc89dd9f712e928573926cfd62b8769d08d1
MS <ms@taler.net>
2020-10-27 15:25:45 +0100

    more jackson testing

--- nexus/src/test/kotlin/JsonTest.kt
+++ nexus/src/test/kotlin/JsonTest.kt
@@ -3,6 +3,7 @@ import org.junit.Test
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
 import tech.libeufin.nexus.server.CreateBankConnectionFromBackupRequestJson
+import tech.libeufin.nexus.server.CreateBankConnectionFromNewRequestJson
 
 
 class JsonTest {
@@ -15,7 +16,11 @@ class JsonTest {
         )
         val roundTrip = mapper.readValue<CreateBankConnectionFromBackupRequestJson>(mapper.writeValueAsString(backupObj))
         assert(roundTrip.data.toString() == "{}" && roundTrip.passphrase == "secret" && roundTrip.name == "backup")
-
+        val newConnectionObj = CreateBankConnectionFromNewRequestJson(
+            name = "new-connection", type = "ebics", data = mapper.readTree("{}")
+        )
+        val roundTripNew = mapper.readValue<CreateBankConnectionFromNewRequestJson>(mapper.writeValueAsString(newConnectionObj))
+        assert(roundTripNew.data.toString() == "{}" && roundTripNew.type == "ebics" && roundTripNew.name == "new-connection")
     }
 
 
