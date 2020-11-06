4b3492c16a349cabcddaf1b63b3f8d1c7a99c5a5
MS <ms@taler.net>
2020-11-06 18:23:58 +0100

    include build-common submodule

---
+++ build-system/configure.py
@@ -0,0 +1,9 @@
+# This configure.py.template file is in the public domain.
+
+from talerbuildconfig import *
+
+b = BuildConfig()
+b.enable_prefix()
+b.enable_configmk()
+b.add_tool(PosixTool("find"))
+b.run()
