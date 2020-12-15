6045f4885b5020fc052b1407965f343e2868ecfc
MS <ms@taler.net>
2020-12-15 01:23:46 +0100

    fix payto-parser

--- util/src/main/kotlin/Payto.kt
+++ util/src/main/kotlin/Payto.kt
@@ -1,7 +1,5 @@
 package tech.libeufin.util
 
-import io.ktor.http.HttpStatusCode
-import tech.libeufin.util.EbicsProtocolError
 import java.net.URI
 
 /**
@@ -10,28 +8,31 @@ import java.net.URI
 data class Payto(
     val name: String,
     val iban: String,
-    val bic: String = "NOTGIVEN"
+    val bic: String
 )
+class InvalidPaytoError(msg: String) : Exception(msg)
 
 fun parsePayto(paytoLine: String): Payto {
+    if (!"^payto://".toRegex().containsMatchIn(paytoLine)) throw InvalidPaytoError("Invalid payto line: $paytoLine")
     val javaParsedUri = try {
         URI(paytoLine)
     } catch (e: java.lang.Exception) {
-        throw EbicsProtocolError(HttpStatusCode.BadRequest, "'${paytoLine}' is not a valid URI")
+        throw InvalidPaytoError("'${paytoLine}' is not a valid URI")
     }
     if (javaParsedUri.scheme != "payto") {
-        throw EbicsProtocolError(HttpStatusCode.BadRequest, "'${paytoLine}' is not payto")
+        throw InvalidPaytoError("'${paytoLine}' is not payto")
     }
-    val iban = javaParsedUri.path.split("/").last()
     val queryStringAsList = javaParsedUri.query.split("&")
     // admit only ONE parameter: receiver-name.
     if (queryStringAsList.size != 1) {
-        throw EbicsProtocolError(HttpStatusCode.BadRequest, "'${paytoLine}' has unsupported query string")
+        throw InvalidPaytoError("'${paytoLine}' has unsupported query string")
     }
     val splitParameter = queryStringAsList.first().split("=")
     if (splitParameter.first() != "receiver-name" && splitParameter.first() != "sender-name") {
-        throw EbicsProtocolError(HttpStatusCode.BadRequest, "'${paytoLine}' has unsupported query string")
+        throw InvalidPaytoError("'${paytoLine}' has unsupported query string")
     }
     val receiverName = splitParameter.last()
-    return Payto(iban = iban, name = receiverName)
+    val split_path = javaParsedUri.path.split("/").filter { it.isNotEmpty() }
+    if (split_path.size != 2) throw InvalidPaytoError("BIC and IBAN are both mandatory ($split_path)")
+    return Payto(iban = split_path[1], bic = split_path[0], name = receiverName)
 }
