a0224f355ca2bc4e6e8aeef671acfd76a9623580
ms <ms@taler.net>
2021-10-23 08:54:04 +0200

    fix option name

--- sandbox/src/main/kotlin/tech/libeufin/sandbox/Main.kt
+++ sandbox/src/main/kotlin/tech/libeufin/sandbox/Main.kt
@@ -203,7 +203,7 @@ class MakeTransaction : CliktCommand("Wire-transfer money between Sandbox bank a
 
     private val creditAccount by option(help = "Label of the bank account receiving the payment").required()
     private val debitAccount by option(help = "Label of the bank account issuing the payment").required()
-    private val demobankArg by option("demobank", help = "Which Demobank books this transaction").default("default")
+    private val demobankArg by option("--demobank", help = "Which Demobank books this transaction").default("default")
     private val amount by argument(help = "Amount, in the \$currency:x.y format")
     private val subjectArg by argument(name = "subject", help = "Payment's subject")
 
