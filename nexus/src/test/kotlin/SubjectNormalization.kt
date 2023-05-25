f1add8dc6e27285d8489aa8684d0710c575498ff
MS <ms@taler.net>
2023-05-25 11:57:26 +0200

    comment

--- nexus/src/test/kotlin/SubjectNormalization.kt
+++ nexus/src/test/kotlin/SubjectNormalization.kt
@@ -29,6 +29,7 @@ class SubjectNormalization {
         val extractedPub = extractReservePubFromSubject(validPub) // has 0s.
         assert(CryptoUtil.checkValidEddsaPublicKey(extractedPub!!))
         val extractedPubWithOs = extractReservePubFromSubject(validPub.replace('0', 'O'))
+        // The "!!" ensures that the extractor did find the reserve pub. with Os instead of zoers.
         assert(CryptoUtil.checkValidEddsaPublicKey(extractedPubWithOs!!))
     }
 }
