aa55a2a99cfcfc9d7f3bfcc6f2a5234d3c1f58e2
MS <ms@taler.net>
2023-07-26 12:29:35 +0200

    SQL refactoring.
    Snake case and double quotes removal around names.

--- database-versioning/new/nexus-0001-refactor.sql
+++ database-versioning/new/nexus-0001-refactor.sql
@@ -6,10 +6,10 @@ SELECT _v.register_patch('nexus-0001', NULL, NULL);
 
 -- start of: user management
 
-CREATE TABLE IF NOT EXISTS NexusUsers
+CREATE TABLE IF NOT EXISTS nexus_users
   (id BIGSERIAL PRIMARY KEY
   ,username TEXT NOT NULL
-  ,"password" TEXT NOT NULL
+  ,password TEXT NOT NULL
   ,superuser BOOLEAN NOT NULL
   );
 
@@ -17,129 +17,129 @@ CREATE TABLE IF NOT EXISTS NexusUsers
 
 -- start of: connection management
 
-CREATE TABLE IF NOT EXISTS NexusBankConnections 
+CREATE TABLE IF NOT EXISTS nexus_bank_connections 
   (id BIGSERIAL PRIMARY KEY
-  ,"connectionId" TEXT NOT NULL
-  ,"type" TEXT NOT NULL
+  ,connection_id TEXT NOT NULL
+  ,type TEXT NOT NULL
   ,dialect TEXT NULL
-  ,"user" BIGINT NOT NULL
-  ,CONSTRAINT fk_nexusbankconnections_user_id FOREIGN KEY ("user") REFERENCES NexusUsers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,user BIGINT NOT NULL
+  ,CONSTRAINT fk_nexusbankconnections_user_id FOREIGN KEY (user) REFERENCES nexus_users(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS NexusEbicsSubscribers
+CREATE TABLE IF NOT EXISTS nexus_ebics_subscribers
   (id BIGSERIAL PRIMARY KEY
-  ,"ebicsURL" TEXT NOT NULL
-  ,"hostID" TEXT NOT NULL
-  ,"partnerID" TEXT NOT NULL
-  ,"userID" TEXT NOT NULL
-  ,"systemID" TEXT NULL
-  ,"signaturePrivateKey" bytea NOT NULL
-  ,"encryptionPrivateKey" bytea NOT NULL
-  ,"authenticationPrivateKey" bytea NOT NULL
-  ,"bankEncryptionPublicKey" bytea NULL
-  ,"bankAuthenticationPublicKey" bytea NULL
-  ,"nexusBankConnection" BIGINT NOT NULL
-  ,"ebicsIniState" VARCHAR(16) NOT NULL
-  ,"ebicsHiaState" VARCHAR(16) NOT NULL
-  ,CONSTRAINT fk_nexusebicssubscribers_nexusbankconnection_id FOREIGN KEY ("nexusBankConnection") REFERENCES NexusBankConnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,ebics_url TEXT NOT NULL
+  ,host_id TEXT NOT NULL
+  ,partner_id TEXT NOT NULL
+  ,user_id TEXT NOT NULL
+  ,system_id TEXT NULL
+  ,signature_private_key bytea NOT NULL
+  ,encryption_private_key bytea NOT NULL
+  ,authentication_private_key bytea NOT NULL
+  ,bank_encryption_public_key bytea NULL
+  ,bank_authentication_public_key bytea NULL
+  ,nexus_bank_connection BIGINT NOT NULL
+  ,ebics_ini_state VARCHAR(16) NOT NULL
+  ,ebics_hia_state VARCHAR(16) NOT NULL
+  ,CONSTRAINT fk_nexusebicssubscribers_nexusbankconnection_id FOREIGN KEY (nexus_bank_connection) REFERENCES nexus_bank_connections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS XLibeufinBankUsers
+CREATE TABLE IF NOT EXISTS xlibeufin_bank_users
   (id BIGSERIAL PRIMARY KEY
   ,username TEXT NOT NULL
-  ,"password" TEXT NOT NULL
-  ,"baseUrl" TEXT NOT NULL
-  ,"nexusBankConnection" BIGINT NOT NULL
-  ,CONSTRAINT fk_xlibeufinbankusers_nexusbankconnection_id FOREIGN KEY ("nexusBankConnection") REFERENCES NexusBankConnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,password TEXT NOT NULL
+  ,base_url TEXT NOT NULL
+  ,nexus_bank_connection BIGINT NOT NULL
+  ,CONSTRAINT fk_xlibeufinbankusers_nexusbankconnection_id FOREIGN KEY (nexus_bank_connection) REFERENCES nexus_bank_connections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS OfferedBankAccounts 
+CREATE TABLE IF NOT EXISTS offered_bank_accounts 
   (id BIGSERIAL PRIMARY KEY
-  ,"offeredAccountId" TEXT NOT NULL
-  ,"bankConnection" BIGINT NOT NULL
+  ,offered_account_id TEXT NOT NULL
+  ,bank_connection BIGINT NOT NULL
   ,iban TEXT NOT NULL
-  ,"bankCode" TEXT NOT NULL
-  ,"holderName" TEXT NOT NULL
+  ,bank_code TEXT NOT NULL
+  ,holder_name TEXT NOT NULL
   ,imported BIGINT NULL
-  ,CONSTRAINT fk_offeredbankaccounts_bankconnection_id FOREIGN KEY ("bankConnection") REFERENCES nexusbankconnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,CONSTRAINT fk_offeredbankaccounts_imported_id FOREIGN KEY (imported) REFERENCES NexusBankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,CONSTRAINT fk_offeredbankaccounts_bankconnection_id FOREIGN KEY (bank_connection) REFERENCES nexusbankconnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,CONSTRAINT fk_offeredbankaccounts_imported_id FOREIGN KEY (imported) REFERENCES nexus_bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
 -- end of: connection management
 
 -- start of: background tasks
 
-CREATE TABLE IF NOT EXISTS NexusScheduledTasks 
+CREATE TABLE IF NOT EXISTS nexus_scheduled_tasks 
   (id BIGSERIAL PRIMARY KEY
-  ,"resourceType" TEXT NOT NULL
-  ,"resourceId" TEXT NOT NULL
-  ,"taskName" TEXT NOT NULL
-  ,"taskType" TEXT NOT NULL
-  ,"taskCronspec" TEXT NOT NULL
-  ,"taskParams" TEXT NOT NULL
-  ,"nextScheduledExecutionSec" BIGINT NULL
-  ,"lastScheduledExecutionSec" BIGINT NULL
+  ,resource_type TEXT NOT NULL
+  ,resource_id TEXT NOT NULL
+  ,task_name TEXT NOT NULL
+  ,task_type TEXT NOT NULL
+  ,task_cronspec TEXT NOT NULL
+  ,task_params TEXT NOT NULL
+  ,next_scheduled_execution_sec BIGINT NULL
+  ,last_scheduled_execution_sec BIGINT NULL
   );
 
 -- end of: background tasks
 
 -- start of: facades management
 
-CREATE TABLE IF NOT EXISTS FacadeState 
+CREATE TABLE IF NOT EXISTS facade_state 
   (id BIGSERIAL PRIMARY KEY
-  ,"bankAccount" TEXT NOT NULL
-  ,"bankConnection" TEXT NOT NULL
+  ,bank_account TEXT NOT NULL
+  ,bank_connection TEXT NOT NULL
   ,currency TEXT NOT NULL
-  ,"reserveTransferLevel" TEXT NOT NULL
+  ,reserve_transfer_level TEXT NOT NULL
   ,facade BIGINT NOT NULL
-  ,"highestSeenMessageSerialId" BIGINT DEFAULT 0 NOT NULL
+  ,highest_seen_message_serial_id BIGINT DEFAULT 0 NOT NULL
   ,CONSTRAINT fk_facadestate_facade_id FOREIGN KEY (facade) REFERENCES facades(id) ON DELETE CASCADE ON UPDATE RESTRICT
   );
 
 CREATE TABLE IF NOT EXISTS facades 
   (id BIGSERIAL PRIMARY KEY
-  ,"facadeName" TEXT NOT NULL UNIQUE
-  ,"type" TEXT NOT NULL
+  ,facade_name TEXT NOT NULL UNIQUE
+  ,type TEXT NOT NULL
   ,creator BIGINT NOT NULL
-  ,CONSTRAINT fk_facades_creator_id FOREIGN KEY (creator) REFERENCES NexusUsers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,CONSTRAINT fk_facades_creator_id FOREIGN KEY (creator) REFERENCES nexus_users(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS NexusPermissions 
+CREATE TABLE IF NOT EXISTS nexus_permissions 
   (id BIGSERIAL PRIMARY KEY
-  ,"resourceType" TEXT NOT NULL
-  ,"resourceId" TEXT NOT NULL
-  ,"subjectType" TEXT NOT NULL
-  ,"subjectName" TEXT NOT NULL
-  ,"permissionName" TEXT NOT NULL
+  ,resource_type TEXT NOT NULL
+  ,resource_id TEXT NOT NULL
+  ,subject_type TEXT NOT NULL
+  ,subject_name TEXT NOT NULL
+  ,permission_name TEXT NOT NULL
   );
 
 -- end of: general facades management
 
 -- start of: Taler facade management
 
-CREATE TABLE IF NOT EXISTS TalerIncomingPayments 
+CREATE TABLE IF NOT EXISTS taler_incoming_payments 
   (id BIGSERIAL PRIMARY KEY
-  ,payment NOT NULL REFERENCES NexusBankTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"reservePublicKey" TEXT NOT NULL
-  ,"timestampMs" BIGINT NOT NULL
-  ,"incomingPaytoUri" TEXT NOT NULL
+  ,payment NOT NULL REFERENCES nexus_bank_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,reserve_public_key TEXT NOT NULL
+  ,timestamp_ms BIGINT NOT NULL
+  ,incoming_payto_uri TEXT NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS TalerRequestedPayments 
+CREATE TABLE IF NOT EXISTS taler_requested_payments 
   (id BIGSERIAL PRIMARY KEY
   ,facade NOT NULL REFERENCES facades(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,payment NOT NULL REFERENCES PaymentInitiations(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"requestUid" TEXT NOT NULL
+  ,payment NOT NULL REFERENCES payment_initiations(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,request_uid TEXT NOT NULL
   ,amount TEXT NOT NULL
-  ,"exchangeBaseUrl" TEXT NOT NULL
+  ,exchange_base_url TEXT NOT NULL
   ,wtid TEXT NOT NULL
-  ,"creditAccount" TEXT NOT NULL
+  ,credit_account TEXT NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS TalerInvalidIncomingPayments 
+CREATE TABLE IF NOT EXISTS taler_invalid_incoming_payments 
   (id BIGSERIAL PRIMARY KEY
-  ,payment NOT NULL REFERENCES NexusBankTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"timestampMs" BIGINT NOT NULL
+  ,payment NOT NULL REFERENCES nexus_bank_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,timestamp_ms BIGINT NOT NULL
   ,refunded BOOLEAN DEFAULT false NOT NULL
   );
 
@@ -147,89 +147,89 @@ CREATE TABLE IF NOT EXISTS TalerInvalidIncomingPayments
 
 -- start of: Anastasis facade management
 
-CREATE TABLE IF NOT EXISTS AnastasisIncomingPayments 
+CREATE TABLE IF NOT EXISTS anastasis_incoming_payments 
   (id BIGSERIAL PRIMARY KEY
-  ,payment NOT NULL REFERENCES NexusBankTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,payment NOT NULL REFERENCES nexus_bank_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   ,subject TEXT NOT NULL
-  ,"timestampMs" BIGINT NOT NULL
-  ,"incomingPaytoUri" TEXT NOT NULL
+  ,timestamp_ms BIGINT NOT NULL
+  ,incoming_payto_uri TEXT NOT NULL
   );
 
 -- end of: Anastasis facade management
 
 -- start of: core banking
 
-CREATE TABLE IF NOT EXISTS NexusBankAccounts
+CREATE TABLE IF NOT EXISTS nexus_bank_accounts
   (id BIGSERIAL PRIMARY KEY
-  ,"bankAccountId" TEXT NOT NULL UNIQUE
-  ,"accountHolder" TEXT NOT NULL
+  ,bank_account_id TEXT NOT NULL UNIQUE
+  ,account_holder TEXT NOT NULL
   ,iban TEXT NOT NULL
-  ,"bankCode" TEXT NOT NULL
-  ,"defaultBankConnection" BIGINT NULL
-  ,"lastStatementCreationTimestamp" BIGINT NULL
-  ,"lastReportCreationTimestamp" BIGINT NULL
-  ,"lastNotificationCreationTimestamp" BIGINT NULL
-  ,"highestSeenBankMessageSerialId" BIGINT NOT NULL
+  ,bank_code TEXT NOT NULL
+  ,default_bank_connection BIGINT NULL
+  ,last_statement_creation_timestamp BIGINT NULL
+  ,last_report_creation_timestamp BIGINT NULL
+  ,last_notification_creation_timestamp BIGINT NULL
+  ,highest_seen_bank_message_serial_id BIGINT NOT NULL
   ,pain001counter BIGINT DEFAULT 1 NOT NULL
-  ,CONSTRAINT fk_nexusbankaccounts_defaultbankconnection_id FOREIGN KEY ("defaultBankConnection") REFERENCES NexusBankConnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,CONSTRAINT fk_nexusbankaccounts_defaultbankconnection_id FOREIGN KEY (default_bank_connection) REFERENCES nexus_bank_connections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS NexusBankTransactions 
+CREATE TABLE IF NOT EXISTS nexus_bank_transactions 
   (id BIGSERIAL PRIMARY KEY
-  ,"accountTransactionId" TEXT NOT NULL
-  ,"bankAccount" NOT NULL REFERENCES NexusBankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"creditDebitIndicator" TEXT NOT NULL
+  ,account_transaction_id TEXT NOT NULL
+  ,bank_account NOT NULL REFERENCES nexus_bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,credit_debit_indicator TEXT NOT NULL
   ,currency TEXT NOT NULL
   ,amount TEXT NOT NULL
   ,status VARCHAR(16) NOT NULL
-  ,"updatedBy" BIGINT NULL REFERENCES NexusBankTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"transactionJson" TEXT NOT NULL
+  ,updated_by BIGINT NULL REFERENCES nexus_bank_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,transaction_json TEXT NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS PaymentInitiations
+CREATE TABLE IF NOT EXISTS payment_initiations
   (id BIGSERIAL PRIMARY KEY
-  ,"bankAccount" NOT NULL REFERENCES NexusBankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"preparationDate" BIGINT NOT NULL
-  ,"submissionDate" BIGINT NULL
-  ,"sum" TEXT NOT NULL
+  ,bank_account NOT NULL REFERENCES nexus_bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,preparation_date BIGINT NOT NULL
+  ,submission_date BIGINT NULL
+  ,sum TEXT NOT NULL
   ,currency TEXT NOT NULL
-  ,"endToEndId" TEXT NOT NULL
-  ,"paymentInformationId" TEXT NOT NULL
-  ,"instructionId" TEXT NOT NULL
+  ,end_to_end_id TEXT NOT NULL
+  ,payment_information_id TEXT NOT NULL
+  ,instruction_id TEXT NOT NULL
   ,subject TEXT NOT NULL
-  ,"creditorIban" TEXT NOT NULL
-  ,"creditorBic" TEXT NULL
-  ,"creditorName" TEXT NOT NULL
+  ,creditor_iban TEXT NOT NULL
+  ,creditor_bic TEXT NULL
+  ,creditor_name TEXT NOT NULL
   ,submitted BOOLEAN DEFAULT false NOT NULL
   ,invalid BOOLEAN NULL
-  ,"messageId" TEXT NOT NULL
-  ,"rawConfirmation" BIGINT NULL REFERENCES NexusBankTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,message_id TEXT NOT NULL
+  ,raw_confirmation BIGINT NULL REFERENCES nexus_bank_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS NexusBankBalances -- table never used
+CREATE TABLE IF NOT EXISTS nexus_bank_balances -- table never used
   (id BIGSERIAL PRIMARY KEY
   ,balance TEXT NOT NULL
-  ,"creditDebitIndicator" TEXT NOT NULL
-  ,"bankAccount" BIGINT NOT NULL
-  ,"date" TEXT NOT NULL
-  ,CONSTRAINT fk_nexusbankbalances_bankaccount_id FOREIGN KEY ("bankAccount") REFERENCES NexusBankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,credit_debit_indicator TEXT NOT NULL
+  ,bank_account BIGINT NOT NULL
+  ,date TEXT NOT NULL
+  ,CONSTRAINT fk_nexusbankbalances_bankaccount_id FOREIGN KEY (bank_account) REFERENCES nexus_bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS NexusBankMessages
+CREATE TABLE IF NOT EXISTS nexus_bank_messages
   (id BIGSERIAL PRIMARY KEY
-  ,"bankConnection" BIGINT NOT NULL
+  ,bank_connection BIGINT NOT NULL
   ,message bytea NOT NULL
-  ,"messageId" TEXT NULL
-  ,"fetchLevel" VARCHAR(16) NOT NULL
+  ,message_id TEXT NULL
+  ,fetch_level VARCHAR(16) NOT NULL
   ,errors BOOLEAN DEFAULT false NOT NULL
-  ,CONSTRAINT fk_nexusbankmessages_bankconnection_id FOREIGN KEY ("bankConnection") REFERENCES NexusBankConnections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,CONSTRAINT fk_nexusbankmessages_bankconnection_id FOREIGN KEY (bank_connection) REFERENCES nexus_bank_connections(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
 ALTER TABLE
-  OfferedBankAccounts ADD CONSTRAINT offeredbankaccounts_offeredaccountid_bankconnection_unique UNIQUE ("offeredAccountId", "bankConnection");
+  offered_bank_accounts ADD CONSTRAINT offeredbankaccounts_offeredaccountid_bankconnection_unique UNIQUE (offered_account_id, bank_connection);
 
 ALTER TABLE
-  NexusPermissions ADD CONSTRAINT nexuspermissions_resourcetype_resourceid_subjecttype_subjectnam UNIQUE ("resourceType", "resourceId", "subjectType", "subjectName", "permissionName");
+  nexus_permissions ADD CONSTRAINT nexuspermissions_resourcetype_resourceid_subjecttype_subjectnam UNIQUE (resource_type, resource_id, subject_type, subject_name, permission_name);
 
 -- end of: core banking
 
--- database-versioning/new/sandbox-0001-refactor.sql
+++ database-versioning/new/sandbox-0001-refactor.sql
@@ -10,96 +10,96 @@ SELECT _v.register_patch('sandbox-0001', NULL, NULL);
 
 -- start of: demobank config tables
 
-CREATE TABLE IF NOT EXISTS DemobankConfigs 
+CREATE TABLE IF NOT EXISTS demobank_configs 
   (id BIGSERIAL PRIMARY KEY
   ,name TEXT NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS DemobankConfigPairs 
+CREATE TABLE IF NOT EXISTS demobank_config_pairs 
   (id BIGSERIAL PRIMARY KEY
-  ,"demobankName" TEXT NOT NULL
-  ,"configKey" TEXT NOT NULL
-  ,"configValue" TEXT NULL
+  ,demobank_name TEXT NOT NULL
+  ,config_key TEXT NOT NULL
+  ,config_value TEXT NULL
   );
 
 -- end of: demobank config tables
 
 -- start of: bank accounts
 
-CREATE TABLE IF NOT EXISTS DemobankCustomers 
+CREATE TABLE IF NOT EXISTS demobank_customers 
   (id BIGSERIAL PRIMARY KEY
   ,username TEXT NOT NULL
-  ,"passwordHash" TEXT NOT NULL
-  ,"name" TEXT NULL
+  ,password_hash TEXT NOT NULL
+  ,name TEXT NULL
   ,email TEXT NULL
   ,phone TEXT NULL
   ,cashout_address TEXT NULL
   );
 
-CREATE TABLE IF NOT EXISTS BankAccounts 
+CREATE TABLE IF NOT EXISTS bank_accounts 
   (id SERIAL PRIMARY KEY
   ,iban TEXT NOT NULL
   ,bic TEXT NOT NULL -- NOTE: This had a default of 'SANDBOXX', now Kotlin must keep it.
-  ,"label" TEXT NOT NULL UNIQUE
-  ,"owner" TEXT NOT NULL
-  ,"isPublic" BOOLEAN DEFAULT false NOT NULL
-  ,"demoBank" FIXME_TYPE REFERENCES DemobankConfigs(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"lastTransaction" FIXME_TYPE NULL REFERENCES BankAccountTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT -- FIXME: under discussion on MM, might be removed.
-  ,"lastFiatSubmission" FIXME_TYPE NULL REFERENCES BankAccountTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"lastFiatFetch" TEXT DEFAULT '0' NOT NULL
-  ,"balance" TEXT DEFAULT '0'
+  ,label TEXT NOT NULL UNIQUE
+  ,owner TEXT NOT NULL
+  ,is_public BOOLEAN DEFAULT false NOT NULL
+  ,demo_bank FIXME_TYPE REFERENCES demobank_configs(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,last_transaction FIXME_TYPE NULL REFERENCES bank_account_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT -- FIXME: under discussion on MM, might be removed.
+  ,last_fiat_submission FIXME_TYPE NULL REFERENCES bank_account_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,last_fiat_fetch TEXT DEFAULT '0' NOT NULL
+  ,balance TEXT DEFAULT '0'
   );
 
 -- end of: bank accounts
 
 -- start of: money transactions
 
-CREATE TABLE IF NOT EXISTS BankAccountTransactions 
+CREATE TABLE IF NOT EXISTS bank_account_transactions 
   (id BIGSERIAL PRIMARY KEY
-  ,"creditorIban" TEXT NOT NULL
-  ,"creditorBic" TEXT NULL
-  ,"creditorName" TEXT NOT NULL
-  ,"debtorIban" TEXT NOT NULL
-  ,"debtorBic" TEXT NULL
-  ,"debtorName" TEXT NOT NULL
+  ,creditor_iban TEXT NOT NULL
+  ,creditor_bic TEXT NULL
+  ,creditor_name TEXT NOT NULL
+  ,debtor_iban TEXT NOT NULL
+  ,debtor_bic TEXT NULL
+  ,debtor_name TEXT NOT NULL
   ,subject TEXT NOT NULL
   ,amount TEXT NOT NULL
   ,currency TEXT NOT NULL
-  ,"date" BIGINT NOT NULL
-  ,"accountServicerReference" TEXT NOT NULL
-  ,"pmtInfId" TEXT NULL
-  ,"EndToEndId" TEXT NULL
+  ,date BIGINT NOT NULL
+  ,account_servicer_reference TEXT NOT NULL
+  ,pmt_inf_id TEXT NULL
+  ,end_to_end_id TEXT NULL
   ,direction TEXT NOT NULL
-  ,account INT NOT NULL REFERENCES BankAccounts(id) ON DELETE CASCADE ON UPDATE RESTRICT
-  ,demobank FIXME_TYPE NOT NULL REFERENCES DemobankConfigs(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,account INT NOT NULL REFERENCES bank_accounts(id) ON DELETE CASCADE ON UPDATE RESTRICT
+  ,demobank FIXME_TYPE NOT NULL REFERENCES demobank_configs(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
 -- end of: money transactions
 
 -- start of: cashout management
 
-CREATE TABLE IF NOT EXISTS CashoutSubmissions 
+CREATE TABLE IF NOT EXISTS cashout_submissions 
   (id BIGSERIAL PRIMARY KEY
-  ,"localTransaction" FIXME_TYPE NOT NULL UNIQUE REFERENCES BankAccountTransactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"maybeNexusResponse" TEXT NULL
-  ,"submissionTime" BIGINT NULL
+  ,local_transaction FIXME_TYPE NOT NULL UNIQUE REFERENCES bank_account_transactions(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,maybe_nexus_response TEXT NULL
+  ,submission_time BIGINT NULL
   );
 
-CREATE TABLE IF NOT EXISTS CashoutOperations 
+CREATE TABLE IF NOT EXISTS cashout_operations 
   (id BIGSERIAL PRIMARY KEY
   ,uuid uuid NOT NULL
-  ,"amountDebit" TEXT NOT NULL
-  ,"amountCredit" TEXT NOT NULL
-  ,"buyAtRatio" TEXT NOT NULL
-  ,"buyInFee" TEXT NOT NULL
-  ,"sellAtRatio" TEXT NOT NULL
-  ,"sellOutFee" TEXT NOT NULL
+  ,amount_debit TEXT NOT NULL
+  ,amount_credit TEXT NOT NULL
+  ,buy_at_ratio TEXT NOT NULL
+  ,buy_in_fee TEXT NOT NULL
+  ,sell_at_ratio TEXT NOT NULL
+  ,sell_out_fee TEXT NOT NULL
   ,subject TEXT NOT NULL
-  ,"creationTime" BIGINT NOT NULL
-  ,"confirmationTime" BIGINT NULL
-  ,"tanChannel" INT NOT NULL
+  ,creation_time BIGINT NOT NULL
+  ,confirmation_time BIGINT NULL
+  ,tan_channel INT NOT NULL
   ,account TEXT NOT NULL
-  ,"cashoutAddress" TEXT NOT NULL
+  ,cashout_address TEXT NOT NULL
   ,tan TEXT NOT NULL
   ,status INT DEFAULT 1 NOT NULL
   );
@@ -108,114 +108,114 @@ CREATE TABLE IF NOT EXISTS CashoutOperations
 
 -- start of: EBICS management
 
-CREATE TABLE IF NOT EXISTS EbicsHosts 
+CREATE TABLE IF NOT EXISTS ebics_hosts 
   (id SERIAL PRIMARY KEY
-  ,"hostID" TEXT NOT NULL
-  ,"ebicsVersion" TEXT NOT NULL
-  ,"signaturePrivateKey" bytea NOT NULL
-  ,"encryptionPrivateKey" bytea NOT NULL
-  ,"authenticationPrivateKey" bytea NOT NULL
+  ,host_id TEXT NOT NULL
+  ,ebics_version TEXT NOT NULL
+  ,signature_private_key bytea NOT NULL
+  ,encryption_private_key bytea NOT NULL
+  ,authentication_private_key bytea NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS EbicsSubscribers 
+CREATE TABLE IF NOT EXISTS ebics_subscribers 
   (id SERIAL PRIMARY KEY
-  ,"userID" TEXT NOT NULL
-  ,"partnerID" TEXT NOT NULL
-  ,"systemID" TEXT NULL
-  ,"hostID" TEXT NOT NULL
-  ,"signatureKey" INT NULL REFERENCES EbicsSubscriberPublicKeys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"encryptionKey" INT NULL REFERENCES EbicsSubscriberPublicKeys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"authorizationKey" INT NULL REFERENCES EbicsSubscriberPublicKeys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"nextOrderID" INT NOT NULL
-  ,"state" INT NOT NULL
-  ,"bankAccount" INT NULL REFERENCES BankAccounts(id) ON DELETE CASCADE ON UPDATE RESTRICT
+  ,user_id TEXT NOT NULL
+  ,partner_id TEXT NOT NULL
+  ,system_id TEXT NULL
+  ,host_id TEXT NOT NULL
+  ,signature_key INT NULL REFERENCES ebics_subscriber_public_keys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,encryption_key INT NULL REFERENCES ebics_subscriber_public_keys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,authorization_key INT NULL REFERENCES ebics_subscriber_public_keys(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,next_order_id INT NOT NULL
+  ,state INT NOT NULL
+  ,bank_account INT NULL REFERENCES bank_accounts(id) ON DELETE CASCADE ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS EbicsSubscriberPublicKeys
+CREATE TABLE IF NOT EXISTS ebics_subscriber_public_keys
   (id SERIAL PRIMARY KEY
-   ,"rsaPublicKey" bytea NOT NULL
-   ,"state" INT NOT NULL
+   ,rsa_public_key bytea NOT NULL
+   ,state INT NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS EbicsDownloadTransactions 
-  ("transactionID" TEXT NOT NULL
-  ,"orderType" TEXT NOT NULL
-  ,host INT NOT NULL REFERENCES EbicsHosts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,subscriber INT NOT NULL REFERENCES EbicsSubscribers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"encodedResponse" TEXT NOT NULL
-  ,"transactionKeyEnc" bytea NOT NULL
-  ,"numSegments" INT NOT NULL
-  ,"segmentSize" INT NOT NULL
-  ,"receiptReceived" BOOLEAN NOT NULL
+CREATE TABLE IF NOT EXISTS ebics_download_transactions 
+  (transaction_id TEXT NOT NULL
+  ,order_type TEXT NOT NULL
+  ,host INT NOT NULL REFERENCES ebics_hosts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,subscriber INT NOT NULL REFERENCES ebics_subscribers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,encoded_response TEXT NOT NULL
+  ,transaction_key_enc bytea NOT NULL
+  ,num_segments INT NOT NULL
+  ,segment_size INT NOT NULL
+  ,receipt_received BOOLEAN NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS EbicsUploadTransactions 
-  ("transactionID" TEXT NOT NULL
-  ,"orderType" TEXT NOT NULL
-  ,"orderID" TEXT NOT NULL
-  ,host INT NOT NULL REFERENCES EbicsHosts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,subscriber INT NOT NULL REFERENCES EbicsSubscribers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"numSegments" INT NOT NULL
-  ,"lastSeenSegment" INT NOT NULL
-  ,"transactionKeyEnc" bytea NOT NULL
+CREATE TABLE IF NOT EXISTS ebics_upload_transactions 
+  (transaction_id TEXT NOT NULL
+  ,order_type TEXT NOT NULL
+  ,order_id TEXT NOT NULL
+  ,host INT NOT NULL REFERENCES ebics_hosts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,subscriber INT NOT NULL REFERENCES ebics_subscribers(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,num_segments INT NOT NULL
+  ,last_seen_segment INT NOT NULL
+  ,transaction_key_enc bytea NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS EbicsUploadTransactionChunks 
-  ("transactionID" TEXT NOT NULL
-  ,"chunkIndex" INT NOT NULL
-  ,"chunkContent" bytea NOT NULL
+CREATE TABLE IF NOT EXISTS ebics_upload_transaction_chunks 
+  (transaction_id TEXT NOT NULL
+  ,chunk_index INT NOT NULL
+  ,chunk_content bytea NOT NULL
   );
 
-CREATE TABLE IF NOT EXISTS EbicsOrderSignatures 
+CREATE TABLE IF NOT EXISTS ebics_order_signatures 
   (id SERIAL PRIMARY KEY
-  ,"orderID" TEXT NOT NULL
-  ,"orderType" TEXT NOT NULL
-  ,"partnerID" TEXT NOT NULL
-  ,"userID" TEXT NOT NULL
-  ,"signatureAlgorithm" TEXT NOT NULL
-  ,"signatureValue" bytea NOT NULL
+  ,order_id TEXT NOT NULL
+  ,order_type TEXT NOT NULL
+  ,partner_id TEXT NOT NULL
+  ,user_id TEXT NOT NULL
+  ,signature_algorithm TEXT NOT NULL
+  ,signature_value bytea NOT NULL
   );
 
 -- end of: EBICS management
 
 -- start of: accounts activity report 
 
-CREATE TABLE IF NOT EXISTS BankAccountFreshTransactions 
+CREATE TABLE IF NOT EXISTS bank_account_fresh_transactions 
   (id BIGSERIAL PRIMARY KEY
-  ,"transaction" FIXME_TYPE NOT NULL REFERENCES BankAccountTransactions(id) ON DELETE CASCADE ON UPDATE RESTRICT
+  ,transaction FIXME_TYPE NOT NULL REFERENCES bank_account_transactions(id) ON DELETE CASCADE ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS BankAccountReports 
+CREATE TABLE IF NOT EXISTS bank_account_reports 
   (id SERIAL PRIMARY KEY
-  ,"reportId" TEXT NOT NULL
-  ,"creationTime" BIGINT NOT NULL
-  ,"xmlMessage" TEXT NOT NULL
-  ,"bankAccount" INT NOT NULL REFERENCES BankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,report_id TEXT NOT NULL
+  ,creation_time BIGINT NOT NULL
+  ,xml_message TEXT NOT NULL
+  ,bank_account INT NOT NULL REFERENCES bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
-CREATE TABLE IF NOT EXISTS BankAccountStatements 
+CREATE TABLE IF NOT EXISTS bank_account_statements 
   (id SERIAL PRIMARY KEY
-  ,"statementId" TEXT NOT NULL
-  ,"creationTime" BIGINT NOT NULL
-  ,"xmlMessage" TEXT NOT NULL
-  ,"bankAccount" INT NOT NULL REFERENCES BankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
-  ,"balanceClbd" TEXT NOT NULL
+  ,statement_id TEXT NOT NULL
+  ,creation_time BIGINT NOT NULL
+  ,xml_message TEXT NOT NULL
+  ,bank_account INT NOT NULL REFERENCES bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,balance_clbd TEXT NOT NULL
   );
 
 -- end of: accounts activity report 
 
 -- start of: Taler integration
 
-CREATE TABLE IF NOT EXISTS TalerWithdrawals 
+CREATE TABLE IF NOT EXISTS taler_withdrawals 
   (id BIGSERIAL PRIMARY KEY
   ,wopid uuid NOT NULL
   ,amount TEXT NOT NULL
-  ,"selectionDone" BOOLEAN DEFAULT false NOT NULL
+  ,selection_done BOOLEAN DEFAULT false NOT NULL
   ,aborted BOOLEAN DEFAULT false NOT NULL
-  ,"confirmationDone" BOOLEAN DEFAULT false NOT NULL
-  ,"reservePub" TEXT NULL
-  ,"selectedExchangePayto" TEXT NULL
-  ,"walletBankAccount" INT NOT NULL REFERENCES BankAccounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
+  ,confirmation_done BOOLEAN DEFAULT false NOT NULL
+  ,reserve_pub TEXT NULL
+  ,selected_exchange_payto TEXT NULL
+  ,wallet_bank_account INT NOT NULL REFERENCES bank_accounts(id) ON DELETE RESTRICT ON UPDATE RESTRICT
   );
 
 -- end of: Taler integration
