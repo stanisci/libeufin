d44aeea974398f536d607d65410eb1d276666e5b
Christian Grothoff <grothoff@gnunet.org>
2023-08-03 15:23:44 +0200

    copy

--- database-versioning/nexus-0001.sql
+++ database-versioning/nexus-0001.sql
@@ -1,3 +1,18 @@
+--
+-- This file is part of TALER
+-- Copyright (C) 2023 Taler Systems SA
+--
+-- TALER is free software; you can redistribute it and/or modify it under the
+-- terms of the GNU General Public License as published by the Free Software
+-- Foundation; either version 3, or (at your option) any later version.
+--
+-- TALER is distributed in the hope that it will be useful, but WITHOUT ANY
+-- WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+-- A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along with
+-- TALER; see the file COPYING.  If not, see <http://www.gnu.org/licenses/>
+--
 -- Compatible with LibEuFin version: 1fe2687aaf696c8566367fe7ed082f1d78e6b78d
 
 BEGIN;
