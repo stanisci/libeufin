042bfec6cbe9c3f732642f6777a94dd4aac5efe6
MS <ms@taler.net>
2023-09-26 11:10:18 +0200

    fix docker launcher config param

--- contrib/docker-launcher/launch-bank.sh
+++ contrib/docker-launcher/launch-bank.sh
@@ -3,5 +3,5 @@
 service postgresql start
 sudo -u postgres createuser -s root
 createdb libeufinbank
-libeufin-bank dbinit
-libeufin-bank serve
+libeufin-bank dbinit -c /libeufin-bank.conf
+libeufin-bank serve -c /libeufin-bank.conf
