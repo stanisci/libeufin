a2e6825908e6fb0271bbc58d849fd6480d013316
Florian Dold <florian@dold.me>
2023-10-06 02:11:55 +0200

    cleanup

--- cli/tests/add-incoming-client.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -v -s \
-  -XPOST \
-  -H "Content-Type: application/json" \
-  -d'{"amount": "MANA:3.00", "reservePub": "re:publica"}' \
-  http://localhost:5001/facades/test-facade/taler-wire-gateway/admin/add-incoming
