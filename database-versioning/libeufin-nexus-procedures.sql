661c1df15b4c86d80beb34a6089c2eb3e74b14a7
MS <ms@taler.net>
2023-10-24 12:25:31 +0200

    installing nexus SQL procedures file

---
+++ database-versioning/libeufin-nexus-procedures.sql
@@ -0,0 +1,41 @@
+BEGIN;
+SET search_path TO libeufin_nexus;
+
+CREATE OR REPLACE FUNCTION bounce_payment(
+  IN in_incoming_transaction_id BIGINT
+  ,IN in_initiation_time BIGINT
+  ,OUT out_nx_incoming_payment BOOLEAN
+)
+LANGUAGE plpgsql AS $$
+BEGIN
+
+INSERT INTO initiated_outgoing_transactions (
+  amount
+  ,wire_transfer_subject
+  ,credit_payto_uri
+  ,in_initiation_time
+  )
+  SELECT
+    amount
+    ,'refund: ' || wire_transfer_subject
+    ,debit_payto_uri
+    ,in_initiation_time
+    FROM incoming_transactions
+    WHERE incoming_transaction_id = in_incoming_transaction_id;
+
+IF NOT FOUND THEN
+  out_nx_incoming_payment=TRUE;
+  RETURN;
+END IF;
+out_nx_incoming_payment=FALSE;
+
+-- finally setting the payment as bounced.  Not checking
+-- the update outcome since the row existence was checked
+-- just above.
+
+UPDATE incoming_transactions
+  SET bounced = true
+  WHERE incoming_transaction_id = in_incoming_transaction_id;
+END $$;
+
+COMMENT ON FUNCTION bounce_payment(BIGINT, BIGINT) IS 'Marks an incoming payment as bounced and initiates its refunding payment';
