e3f73fab8385eecd6be584b2bedee700a13313d7
Christian Grothoff <grothoff@gnunet.org>
2023-11-27 07:27:53 +0900

    -use symlink for settings.js

---
+++ debian/etc/libeufin/settings.js
@@ -0,0 +1,8 @@
+globalThis.talerDemobankSettings = {
+  // Only Admin adds users
+  allowRegistrations: false,
+  bankName: "Taler Bank",
+  // Show explainer text and navbar to other demo sites
+  showDemoNav: false,
+  backendBaseURL: "http://localhost/"
+};
--- debian/usr/share/libeufin-bank/spa/settings.js
+++ debian/usr/share/libeufin-bank/spa/settings.js
@@ -1,8 +1,1 @@
-globalThis.talerDemobankSettings = {
-  // Only Admin adds users
-  allowRegistrations: false,
-  bankName: "Taler Bank",
-  // Show explainer text and navbar to other demo sites
-  showDemoNav: false,
-  backendBaseURL: "http://localhost/"
-};
+../../../../etc/libeufin/settings.js
