af3a5e50d7d25011db76c5a2e694577217841a94
Sebastian <sebasjm@gmail.com>
2023-11-27 09:25:52 -0300

    settings example

--- debian/etc/libeufin/settings.js
+++
@@ -1,8 +0,0 @@
-globalThis.talerDemobankSettings = {
-  // Only Admin adds users
-  allowRegistrations: false,
-  bankName: "Taler Bank",
-  // Show explainer text and navbar to other demo sites
-  showDemoNav: false,
-  backendBaseURL: "http://localhost/"
-};
