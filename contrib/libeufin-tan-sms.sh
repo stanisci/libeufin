da2aa56eaf8ad545746eb7c8520b665980127523
MS <ms@taler.net>
2023-12-10 17:20:00 +0100

    Drop leading + before POSTing to Telesign.

--- contrib/libeufin-tan-sms.sh
+++ contrib/libeufin-tan-sms.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # This file is in the public domain.
 
 set -eu
@@ -8,13 +9,13 @@ set -eu
 
 MESSAGE=`cat -`
 TMPFILE=`mktemp /tmp/sms-loggingXXXXXX`
-
+PHONE_NUMBER=$(echo $1 | sed 's/^+//') # Telesign refuses the leading +
 STATUS=$(curl --request POST \
      --user "$CUSTOMER_ID:$API_KEY" \
      --url https://rest-api.telesign.com/v1/messaging \
      --data "message_type=OTP" \
      --data "message=$MESSAGE" \
-     --data "phone_number=$1" \
+     --data "phone_number=$PHONE_NUMBER" \
      -w "%{http_code}" -s -o $TMPFILE)
 echo `cat $TMPFILE` >> $HOME/sms.log
 rm -f $TMPFILE
