1aec042373819355cd77e45c1ca08cebeb42b889
Christian Grothoff <christian@grothoff.org>
2024-02-12 09:57:10 +0100

    -typo

--- bank/src/main/kotlin/tech/libeufin/bank/Main.kt
+++ bank/src/main/kotlin/tech/libeufin/bank/Main.kt
@@ -62,7 +62,7 @@ private val logger: Logger = LoggerFactory.getLogger("libeufin-bank")
 var engine: ApplicationEngine? = null 
 
 /**
- * This plugin check for body lenght limit and inflates the requests that have "Content-Encoding: deflate"
+ * This plugin check for body length limit and inflates the requests that have "Content-Encoding: deflate"
  */
 val bodyPlugin = createApplicationPlugin("BodyLimitAndDecompression") {
     onCallReceive { call ->
