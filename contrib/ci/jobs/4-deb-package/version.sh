fc72eb5f8b3e82bca93b90c292848e5b731cee9d
Devan Carpenter <devan@taler.net>
2024-04-09 07:57:41 -0400

    ci: remove the leading v character from tags
    debian doesn't want a v at the start of the version string

--- contrib/ci/jobs/4-deb-package/version.sh
+++ contrib/ci/jobs/4-deb-package/version.sh
@@ -10,7 +10,7 @@ else
 	RECENT_VERSION_TAG=$(git describe --tags --match 'v*.*.*' --always --abbrev=0 HEAD || exit 1)
 	commits="$(git rev-list ${RECENT_VERSION_TAG}..HEAD --count)"
 	if [ "${commits}" = "0" ]; then
-		git describe --tag HEAD || exit 1
+		git describe --tag HEAD | sed -r 's/^v//' || exit 1
 	else
 		echo $(echo ${RECENT_VERSION_TAG} | sed -r 's/^v//')-${commits}-$(git rev-parse --short=8 HEAD)
 	fi
